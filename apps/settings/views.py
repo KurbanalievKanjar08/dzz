b226011e3a68e361bef7ce444b4eef9ec75853f8
KurbanalievKanjar08 <kurbanalievkanjar918@gmail.com>
2025-11-13 10:37:15 +0300

    первый коммит

--- apps/settings/views.py
+++ apps/settings/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from apps.settings.models import Settings
 # Create your views here.
 
-from django.shortcuts import render
-
 def index(request):
-    return render(request, 'index.html')
+    settings = Settings.objects.latest('id')
+    return render(request, 'index.html', {'settings': settings})
 
 def about(request):
-    return render(request, 'about.html')
+    settings = Settings.objects.latest('id')
+    return render(request, 'about.html', {'settings': settings})
 
 def contact(request):
-    return render(request, 'contact.html')
+    settings = Settings.objects.latest('id')
+    return render(request, 'contact.html', {'settings': settings})
